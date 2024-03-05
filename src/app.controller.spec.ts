41080174c9137bf58cfd3b57a9329316c00bcfbd
shwephuehmone25 <shwephue7889@gmail.com>
2024-03-05 09:23:46 +0630

    set up with typscript

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello, this is your example data!"', () => {
+      expect(appController.getExampleData()).toBe(
+        'Hello, this is your example data!',
+      );
     });
   });
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MetricsController } from './metrics.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, MetricsController],
   providers: [AppService],
 })
 export class AppModule {}
